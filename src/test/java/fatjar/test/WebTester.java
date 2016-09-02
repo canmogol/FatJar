9121a138c6ddb43c8e582cef6597cc9777ed91f0
Can A. MOGOL <canmogol@gmail.com>
2016-09-03 00:12:03 +0300

    query param example added

--- src/test/java/fatjar/test/WebTester.java
+++ src/test/java/fatjar/test/WebTester.java
@@ -10,6 +10,7 @@ public class WebTester {
     }
 
     public void serverStartTest() {
+        // do a request to http://localhost:8080/ and/or http://localhost:8080/Hi
         UndertowServer.create()
                 .listen(8080, "localhost")
                 .get("/", (req, res) -> {
@@ -17,7 +18,11 @@ public class WebTester {
                     res.write();
                 })
                 .get("/Hi", (req, res) -> {
-                    res.setContent("Hi from get");
+                    if (req.getParams().containsKey("name")) {
+                        res.setContent("Hello " + req.getParams().getValue("name"));
+                    } else {
+                        res.setContent("type \"http://localhost:8080/Hi?name=john\" in your browser");
+                    }
                     res.write();
                 })
                 .post("/", (req, res) -> {

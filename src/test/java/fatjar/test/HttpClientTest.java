0081a3e1c9e754e8069dfbb7e276d5f6f32ca3dd
Ali Can Albayrak <albayrakalican@gmail.com>
2016-11-14 11:15:41 +0300

    test added for HttpClient

---
+++ src/test/java/fatjar/test/HttpClientTest.java
@@ -0,0 +1,16 @@
+package fatjar.test;
+
+import fatjar.HttpClient;
+import fatjar.dto.HttpMethod;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HttpClientTest {
+
+	@Test
+	public void connectionTest() throws HttpClient.HttpClientException {
+		String content = HttpClient.create().url("http://echo.jsontest.com/key/value/one/two").method(HttpMethod.GET).send().getContentAsString().replace("\n", "");
+		Assert.assertEquals(content, "{   \"one\": \"two\",   \"key\": \"value\"}");
+	}
+
+}
